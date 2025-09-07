c261d3c18f06a204d6f1e45a3b4773655b88706d
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-09-07 23:12:09 +0100

    chore(ci): curl verbose

--- .github/tools/submit-jenkins.sh
+++ .github/tools/submit-jenkins.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl 'https://jenkins.service.isnt-a.top/job/Buxus%20update%20app/buildWithParameters' \
+curl 'https://jenkins.service.isnt-a.top/job/Buxus%20update%20app/buildWithParameters' -v \
   --user $JENKINS_USER:$JENKINS_TOKEN \
   --data APP_NAME=ashhhleyyy-dot-dev \
   --data IMAGE_NAME=ghcr.io/ashhhleyyy/website \
